5255594348165648a4be4d38fb303e4150f481b0
Victoria <vlholland@my365.bellevue.edu>
2026-03-21 17:19:41 -0400

    Added HelloWorldApp for Module 1.

---
+++ Module_1/HelloWorldApp/HollandHelloWorld.java
@@ -0,0 +1,7 @@
+public class HollandHelloWorld {
+    public static void main(String[] args) {
+        String firstName = "Victoria";
+        String lastName = "Holland";
+        System.out.printf("Hello World from %s %s%n", firstName, lastName);
+    }
+}
